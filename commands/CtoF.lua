a184dc945a5601b3bb9bdeeeb9b99b1d7d834209
Rens Althuis <rens.althuis@gmail.com>
2018-07-20 04:46:15 +0200

    Added temp conversions

---
+++ commands/CtoF.lua
@@ -0,0 +1,69 @@
+local function CtoF(n)
+    return n*9/5 + 32
+end
+
+local function run(message, content)
+    num = string.match(content, "(%d+)")
+    if num ~= nil then 
+        celcius = CtoF(num)
+        message.channel:send(tostring(celcius))
+    else
+        message.channel:send("thats not a number >:C")
+    end
+    client:emit("messageFinished")
+end
+
+local function del(message, content)
+    print('    Deleting message')
+    local res = message.channel:getMessagesAfter(message, 1)
+    local mess = nil
+
+    for v in res:iter() do
+        mess = v
+    end
+
+    if mess == nil then
+        print('    no message found')
+    else
+        print('    message found: ' .. mess.content)
+        if mess.author.name == client.user.username then
+            mess.delete(mess)
+        end
+
+    end
+
+    client:emit("messageFinished")
+end
+
+local function update(message, content)
+    print('    Updating message')
+    local res = message.channel:getMessagesAfter(message, 1)
+    local mess = nil
+
+    for v in res:iter() do
+        mess = v
+    end
+
+    if mess == nil then 
+        print('    no message found')
+    else
+        if mess.author.name == client.user.username then
+            --update the message
+            num = string.match(content, "(%d+)")
+            celcius = CtoF(num)
+            mess:setContent(tostring(celcius))
+        else
+            print('    no message found')
+        end
+    end
+    client:emit("messageFinished")
+end
+
+return{
+	run = run,
+	del = del,
+	update = update,
+	['trigger'] = "CtoF",
+	['isOn'] = true
+}
+
